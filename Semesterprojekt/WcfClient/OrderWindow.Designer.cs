edf7a13ad4460108ab209ebcaaa0937eb505b66c
Daniel <daniel5078@hotmail.com>
2017-11-13 11:31:48 +0100

    WCFClient + orderwindow added
    WCFClient + orderwindow added

---
+++ Semesterprojekt/WcfClient/OrderWindow.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WcfClient
+{
+    partial class OrderWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // OrderWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1175, 647);
+            this.Name = "OrderWindow";
+            this.Text = "OrderWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
