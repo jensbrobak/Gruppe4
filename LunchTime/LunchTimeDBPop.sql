0c377fc95b0167a1b77dd7fc26fad134d4a473f2
stormkim2105 <stormkim2105@gmail.com>
2017-11-16 11:29:17 +0100

    sql pop
    script to pop db

---
+++ LunchTime/LunchTimeDBPop.sql
@@ -0,0 +1,9 @@
+insert into Products values('Jordbær lagkage', 'Lagkage', 12.50, 8);
+insert into Products values('Sandwtch med kylling og beacon', 'Sandwitch', 15.50, 10);
+insert into Products values('Sandwtch med tunsalat', 'Sandwitch', 15.50, 6);
+insert into Products values('Sandwich med røget laks', 'Sandwitch', 15.50, 4);
+insert into Products values('Sandwich med ribbensteg', 'Sandwitch', 15.50, 2);
+insert into Products values('Toast med skinke og ost', 'Toast', 7.0, 9);
+insert into Products values('Krydret tomat pizza', 'Pizza', 35.50, 15);
+
+insert into Customers values(30.0, 'Joe', 1234);
